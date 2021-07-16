cc5a6d3cc4800a4c2c9874a8ae189df96423fa16
mersadcam <mersadcam@pop-os.localdomain>
2021-07-17 03:28:12 +0430

    edit info and about page has been added

--- Main project/profile/profile.py
+++ Main project/profile/profile.py
@@ -1,8 +1,10 @@
 from PySide6.QtWidgets import QMainWindow, QApplication, QVBoxLayout, QWidget
-from PySide6.QtCore import Qt
+from PySide2.QtCore import QObject, Signal, Slot
 from ui_profile import Ui_Profile
-from about import About
+from editInfo import EditInfo, Country
+from contactInfo import ContactInfo
 import sys
+import datetime
 
 data = {
     "about-content": '''
@@ -13,16 +15,100 @@ data = {
 
 
 class Profile(QMainWindow):
+    editInfo = edit_saved = Signal(str, str, str, int, str, datetime.datetime, str, str)
+
     def __init__(self):
         super(Profile, self).__init__()
         self.ui = Ui_Profile()
         self.ui.setupUi(self)
 
+        self.editInfo_dialog = EditInfo()
+        self.contactInfo_dialog = ContactInfo()
 
+        self.contact_info_email = None
+        self.contact_info_addr = None
+        self.contact_info_bd = None
+        self.contact_info_link = None
 
-        # self.ui.scrollArea.setWidget(fortest)
+        # Connections:
+        self.ui.editInfo_pushButton.clicked.connect(self.editInfo_pushButton_onClicked)
+        self.ui.contactInfo_pushButton.clicked.connect(self.contactInfo_pushButton_onClicked)
+        self.editInfo_dialog.edit_saved.connect(self.info_edited)
 
-        # x = fortest.verticalLayout
+    # public method
+    def set_contact_info(self, email, addr, birthday, link):
+        self.contact_info_link = link
+        self.contact_info_email = email
+        self.contact_info_addr = addr
+        self.contact_info_bd = birthday
+
+    # Ui set Methods:
+    def set_connection_numbers(self, connection_numbers):
+        text = self.ui.connections_pushButton.text()
+        self.ui.connections_pushButton.setText(f"{connection_numbers} {text}")
+
+    def set_country(self, country):
+        self.ui.country_label.setText(country)
+
+    def set_name(self, first_name, last_name):
+        self.ui.firstName_label.setText(first_name)
+        self.ui.lastName_label.setText(last_name)
+
+    def set_about_content(self, about_content):
+        self.ui.aboutContent_label.setText(about_content)
+
+    def set_headline_label(self, headline):
+        self.ui.headline_label.setText(headline)
+
+    # Public slots:
+    @Slot(str, str, str, int, str, datetime.datetime, str, str)
+    def info_edited(self, firstname, lastname, headline,
+                    country, addr, birthday, email_addr, link):
+        # This one should be connected to center class slot
+        self.editInfo.emit(firstname, lastname, headline, country, addr,
+                           birthday, addr, birthday, email_addr, link)
+        # If data changed successfully, changes will be showed by center class
+
+    # Private slots:
+    @Slot()
+    def editInfo_pushButton_onClicked(self):
+        ui = self.ui
+        firstname = ui.firstName_label.text()
+        lastname = ui.lastName_label.text()
+        headline = ui.headline_label.text()
+        country = Country.case(ui.country_label.text())
+        self.editInfo_dialog.fill_fields(firstname, lastname, headline, country, self.contact_info_addr,
+                                         self.contact_info_bd, self.contact_info_email, self.contact_info_link)
+
+        self.editInfo_dialog.show()
+
+    @Slot()
+    def skills_pushButton_onClicked(self):
+        pass
+
+    @Slot()
+    def background_pushButton_onClicked(self):
+        pass
+
+    @Slot()
+    def accomp_pushButton_onClicked(self):
+        pass
+
+    @Slot()
+    def contactInfo_pushButton_onClicked(self):
+        ui = self.ui
+        firstname = ui.firstName_label.text()
+        lastname = ui.lastName_label.text()
+        headline = ui.headline_label.text()
+        country = Country.case(ui.country_label.text())
+
+        self.contactInfo_dialog.fill_fields(firstname, lastname, headline, country, self.contact_info_addr,
+                                            self.contact_info_bd, self.contact_info_email, self.contact_info_link)
+        self.contactInfo_dialog.show()
+
+    @Slot()
+    def connections_pushButton_onClicked(self):
+        pass
 
 
 if __name__ == "__main__":
