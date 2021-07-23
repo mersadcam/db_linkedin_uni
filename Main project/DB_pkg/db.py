010dee82c3d24596cdd4b0362bd0f401678e0687
amirhosein <amirhoseinshahrizi@gmail.com>
2021-07-24 01:01:59 +0430

    bug fix

--- Main project/DB_pkg/db.py
+++ Main project/DB_pkg/db.py
@@ -220,6 +220,7 @@ class User:
     #table creation
     def initialise_User(self):
         try:
+            self.db_cursor.execute(constants.ENABLE_FOREIGN_KEY)
             self.db_cursor.execute(constants.CREATE_TABLE_USER)
             self.db_cursor.execute(constants.CREATE_TABLE_PROFILE)
             self.db_cursor.execute(constants.CREATE_TABLE_CONNECTIONS)
@@ -363,7 +364,7 @@ class User:
 
    
     #PROFILE METHODS
-    def profile_insert(self, user_uuid, profile_first_name=None, profile_last_name=None, profile_headline=None, profile_country=None, profile_birthday=None, profile_address=None, profile_about=None, profile_link=None):
+    def profile_insert(self, user_uuid, profile_first_name=None, profile_last_name=None, profile_headline=None, profile_country=None, profile_birthday='0001-01-01', profile_address=None, profile_about=None, profile_link=None):
         insert_table_values = (profile_first_name, profile_last_name, profile_headline, profile_country, profile_birthday, profile_address, profile_about, profile_link, user_uuid)
         try:
             self.db_cursor.execute(constants.INSERT_RECORD_PROFILE, insert_table_values)
@@ -574,9 +575,10 @@ if __name__ == '__main__':
         # a = user.skill_get_all_user_skills('0')
         # user.skill_remove_user_skills('0', ['12423', '11423', '12314'])
         # user.skill_insert_user_skill('0', ['12423', '11423', '12314'])
+        # user.user_signUp('amir', 'sh', 'amir@gmail.com', '123')
+        user.profile_select('4ace9200-5b2f-4785-8833-2f4552edd62f')
+        # print(datetime.date(1, 1, 1))
 
-
-        # print(a)
 
         db_connection.close()
     except Error as e:
